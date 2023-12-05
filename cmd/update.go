4e84d8c4528235de51d1581e379d4caf4f07e9fd
Massimo Triassi <kidmax14@hotmail.com>
2023-12-05 14:57:44 -0500

    feat: #8 add command to loop saved addons and update them

--- cmd/update.go
+++ cmd/update.go
@@ -5,8 +5,9 @@ package cmd
 
 import (
 	"fmt"
-
+	"github.com/m-triassi/wowforge-cli/pkg/curseforge"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // updateCmd represents the update command
@@ -14,11 +15,28 @@ var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update all tracked addons",
 	Long: `Fetches the latest version of all addons currently being tracked by wowforge-cli. 
-Simply run the command and all files will be downloaded and unpacked: 
-wowforge-cli update
+Simply run the command and all files will be downloaded and unpacked.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("update called")
+		addons := viper.GetIntSlice("addons")
+
+		for _, id := range addons {
+			files, err := curseforge.GetFiles(id)
+			if err != nil {
+				panic(fmt.Errorf("Failed to fetch files from CurseForge, mod id (%d) may not exist: %w", id, err))
+			}
+
+			file, err := curseforge.DownloadFile(id, curseforge.NegotiateFile(files))
+			if err != nil {
+				panic(fmt.Errorf("Could not download file: %w", err))
+			}
+
+			dest := viper.GetString("install")
+			err = curseforge.InstallAddon(file, dest)
+			if err != nil {
+				panic(fmt.Errorf("Failed to install addon in target destination"))
+			}
+		}
 	},
 }
 
