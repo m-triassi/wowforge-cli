31db9d8940898c019433dda5fa4aac14e5b7ac8f
Massimo Triassi <kidmax14@hotmail.com>
2026-09-22 23:37:09 -0400

    feat: #31 store installed addon folders in config to improve readability

--- cmd/update.go
+++ cmd/update.go
@@ -5,22 +5,25 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/m-triassi/wowforge-cli/internal/search"
 	"github.com/m-triassi/wowforge-cli/pkg/curseforge"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"strconv"
 )
 
 // updateCmd represents the update command
 var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update all tracked addons",
-	Long: `Fetches the latest version of all addons currently being tracked by wowforge-cli. 
+	Long: `Fetches the latest version of all addons currently being tracked by wowforge-cli.
 Simply run the command and all files will be downloaded and unpacked.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		addons := viper.GetIntSlice("addons")
+		addons := search.LoadAddons()
 
-		for _, id := range addons {
+		for key := range addons {
+			id, _ := strconv.Atoi(key)
 			files, err := curseforge.GetFiles(id)
 			if err != nil {
 				panic(fmt.Errorf("Failed to fetch files from CurseForge, mod id (%d) may not exist: %w", id, err))
@@ -39,11 +42,14 @@ Simply run the command and all files will be downloaded and unpacked.
 
 			fmt.Printf("Unpacking... ")
 			dest := viper.GetString("install")
-			err = curseforge.InstallAddon(file, dest)
+			folders, err := curseforge.InstallAddon(file, dest)
 			if err != nil {
 				panic(fmt.Errorf("Failed to install addon in target destination"))
 			}
 
+			addons[key] = folders
+			viper.Set("addons", addons)
+			viper.WriteConfig()
 			fmt.Printf("[INSTALLED]\n")
 		}
 	},
--- internal/search/search.go
+++ internal/search/search.go
@@ -1,14 +1,32 @@
 package search
 
-func Contains(haystack []int, needle int) bool {
-	return Find(haystack, needle) >= 0
-}
+import (
+	"fmt"
+	"strconv"
 
-func Find(haystack []int, needle int) int {
-	for index, value := range haystack {
-		if value == needle {
-			return index
+	"github.com/spf13/viper"
+)
+
+// LoadAddons returns the tracked addons as a map of id -> installed folder
+// names. Legacy config shapes ([]int, or id->name string) are migrated in
+// place with empty folder lists that self-heal on the next add/update.
+func LoadAddons() map[string][]string {
+	m := map[string][]string{}
+	for key, val := range viper.GetStringMap("addons") {
+		if folders, ok := val.([]interface{}); ok {
+			for _, f := range folders {
+				m[key] = append(m[key], fmt.Sprint(f))
+			}
+		} else {
+			m[key] = []string{}
 		}
 	}
-	return -1
+	if len(m) > 0 {
+		return m
+	}
+
+	for _, id := range viper.GetIntSlice("addons") {
+		m[strconv.Itoa(id)] = []string{}
+	}
+	return m
 }
